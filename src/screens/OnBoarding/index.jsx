1103e304a313c797c6a475360dc06e8974bf2e40
Shaolong Li <shaolongli26@gmail.com>
2023-02-24 12:03:08 -0800

    Chnage text finish

--- src/screens/OnBoarding/index.jsx
+++ src/screens/OnBoarding/index.jsx
@@ -10,14 +10,28 @@ import LongBottom from '../../components/LongButton';
 import FormInput from '../../components/FormInput';
 import Fontisto from 'react-native-vector-icons/Fontisto';
 import Feather from 'react-native-vector-icons/Feather';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Entypo from 'react-native-vector-icons/Entypo';
 
 const OnBoarding = () => {
   const auth = React.useContext(AuthContext);
   const logoUrl = require('../../assets/images/logo/logo_text_at_bottom.png');
-  const riderURL = require("../../assets/images/on_boarding/matched_on_boarding_get_match.png");
+  const riderURLOne = require("../../assets/images/on_boarding/matched_on_boarding_get_match.png");
+  const riderURLTwo = require("../../assets/images/on_boarding/driver_on_boarding_driver.png");
   const onLogoutPressed = async () => {
     await deleteJWToken();
     auth.signOut();
+  };
+ 
+  const [sloganText, sloganSetText] = useState("Get Matched!");
+  const [tipText, tipSetText] = useState("Schedule you trip ahead and get a match with you potential driver/rider.");
+  // const [imgStyle, imgSetStyle] = useState({});
+  const [riderUrl, riderUrlSetPic] = useState(riderURLOne);
+
+  const onPressHandler = (event) => {
+    sloganSetText("Certified Driver and Rider");
+    tipSetText("All drivers and riders are certified UCSD students.");
+    riderUrlSetPic(riderURLTwo);
   };
 
   return (
@@ -25,10 +39,19 @@ const OnBoarding = () => {
       <View style={styles.root}>
       <Text style={styles.skip}>Skip</Text>
         <Image source={logoUrl} style={styles.logo} />
-        <Text style={styles.slogan}>Get Matched!</Text>
-        <Image source={riderURL} style={styles.rider} />
-        <Text style={styles.tip}>Schedule you trip ahead and get a match 
-        with you potential driver/rider. </Text>
+        <Text style={styles.slogan}>{sloganText}</Text>
+
+        <Image source={riderUrl} style={styles.rider} />
+
+        <Text style={styles.tip}>{tipText}</Text>
+
+        <AntDesign name="rightcircle" style={styles.rightArrow} title="Change Text"
+        onPress={onPressHandler}></AntDesign>
+
+      </View>
+      <View style={styles.dot}>
+        <Entypo name="dot-single" style={styles.dotOne}></Entypo>
+        <Entypo name="dot-single" style={styles.dotTwo}></Entypo>
       </View>
     </SafeAreaView>
   );
--- src/screens/OnBoarding/style.jsx
+++ src/screens/OnBoarding/style.jsx
@@ -3,19 +3,19 @@ import {StyleSheet} from 'react-native';
 const styles = StyleSheet.create({
   root: {
     alignItems: 'center',
-    padding: 38,
+    padding: 20,
   },
   skip: {
     fontSize: 18,
     fontWeight: '200',
-    marginLeft:282,
+    marginRight:-320,
   },
   logo: {
     width: 50,
     height: 64,
   },
   slogan: {
-    marginTop: 100,
+    marginTop: 60,
     fontSize: 38,
     fontWeight: '800',
   },
@@ -28,6 +28,26 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: '500',
     color: 'black',
+  },
+  rightArrow: {
+    fontSize: 40,
+    color: '#172B54',
+    marginTop: 25,
+    marginRight:-310,
+  },
+  dot: {
+    flexDirection:'row',
+  },
+  dotOne: {
+    color: '#171616',
+    marginLeft: 165,
+    fontSize: 40,
+  },
+  dotTwo: {
+    marginLeft: -20,
+    padding: -10,
+    color: '#D9D9D9',
+    fontSize: 40,
   }
 });
 
