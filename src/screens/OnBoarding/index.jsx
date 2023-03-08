6997b6d6537342323dee9087abaf8bc9ef23578a
Shaolong Li <shaolongli26@gmail.com>
2023-03-07 18:15:41 -0800

    temporarily fix onboarding flat list

--- src/screens/OnBoarding/index.jsx
+++ src/screens/OnBoarding/index.jsx
@@ -1,57 +1,213 @@
-import React, {useState} from 'react';
-import {Image, Linking, SafeAreaView, Text, View} from 'react-native';
-import {deleteJWToken} from '../../utils/jwt';
-import {AuthContext} from '../../navigation/navigation';
-import CheckBox from 'react-native';
-import styles from './style';
-import {ucsdEmailRegex} from '../../constants';
-import {useNavigation} from '@react-navigation/core';
-import LongBottom from '../../components/LongButton';
-import FormInput from '../../components/FormInput';
-import Fontisto from 'react-native-vector-icons/Fontisto';
-import Feather from 'react-native-vector-icons/Feather';
-import AntDesign from 'react-native-vector-icons/AntDesign';
-import Entypo from 'react-native-vector-icons/Entypo';
-import {SCREEN_WIDTH, SCREEN_HEIGHT} from '../../constants/constants';
+import React from 'react';
+import {
+  SafeAreaView,
+  Image,
+  StyleSheet,
+  FlatList,
+  View,
+  Text,
+  StatusBar,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
 
-let first_click = true;
-const OnBoarding = () => {
-    const auth = React.useContext(AuthContext);
-    const logoImg = require('../../assets/images/logo/logo_text_at_bottom.png');
-    const driverImgOne = require('../../assets/images/on_boarding/matched_on_boarding_get_match.png');
-    const navigation = useNavigation();
+const {width, height} = Dimensions.get('window');
 
-    const onLogoutPressed = async () => {
-      await deleteJWToken();
-      auth.signOut();
-    };
+const COLORS = {primary: '#282534', white: '#fff'};
 
-    return (
-        <SafeAreaView>
-            <View style={styles.root}>
-                <Text style={styles.skip} onPress={() => {
-                        navigation.navigate('GetStarted');
-                    }}>Skip
-                </Text>
-                <Text style={styles.slogan}>Get Matched!</Text>
-                <Image source={logoImg} style={styles.logo} />
-                <Image source={driverImgOne} style={styles.driverOne}/>
-                <Text style={styles.tip}>Schedule you trip ahead 
-                and get a match with you potential driver/rider.</Text>
-                <AntDesign
-                    name="rightcircle"
-                    style={styles.rightArrow}
-                    onPress={() => {
-                        navigation.navigate('OnBoarding1');
-                    }}
-                />
-                <View style={styles.dot}>
-                    <Entypo name="dot-single" style={styles.dotOne} />
-                    <Entypo name="dot-single" style={styles.dotTwo} />
-                </View>
-            </View>
-        </SafeAreaView>
-    );
+const slides = [
+  {
+    id: '1',
+    image: require('../images/test3.png'),
+    title: 'Best Digital Solution',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    id: '2',
+    image: require('../../assets/images/on_boarding/driver_on_boarding_driver.png'),
+    title: 'Achieve Your Goals',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+];
+
+const Slide = ({item}) => {
+  return (
+    <View style={{alignItems: 'center'}}>
+      <Text style={styles.title}>{item?.title}</Text>
+      <Image
+        source={item?.image}
+        style={{height: '75%', width, resizeMode: 'contain'}}
+      />
+      <View>
+        
+        <Text style={styles.subtitle}>{item?.subtitle}</Text>
+      </View>
+    </View>
+  );
 };
 
+const OnBoarding = () => {
+  const [currentSlideIndex, setCurrentSlideIndex] = React.useState(0);
+  const ref = React.useRef();
+  const updateCurrentSlideIndex = e => {
+    const contentOffsetX = e.nativeEvent.contentOffset.x;
+    const currentIndex = Math.round(contentOffsetX / width);
+    setCurrentSlideIndex(currentIndex);
+  };
+
+  const goToNextSlide = () => {
+    const nextSlideIndex = currentSlideIndex + 1;
+    if (nextSlideIndex != slides.length) {
+      const offset = nextSlideIndex * width;
+      ref?.current.scrollToOffset({offset});
+      setCurrentSlideIndex(currentSlideIndex + 1);
+    }
+  };
+
+  const skip = () => {
+    const lastSlideIndex = slides.length - 1;
+    const offset = lastSlideIndex * width;
+    ref?.current.scrollToOffset({offset});
+    setCurrentSlideIndex(lastSlideIndex);
+  };
+
+  const Footer = () => {
+    return (
+      <View
+        style={{
+          height: height * 0.25,
+          justifyContent: 'space-between',
+          paddingHorizontal: 20,
+        }}>
+        {/* Indicator container */}
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            marginTop: 20,
+          }}>
+          {/* Render indicator */}
+          {slides.map((_, index) => (
+            <View
+              key={index}
+              style={[
+                styles.indicator,
+                currentSlideIndex == index && {
+                  backgroundColor: COLORS.white,
+                  width: 25,
+                },
+              ]}
+            />
+          ))}
+        </View>
+
+        {/* Render buttons */}
+        <View style={{marginBottom: 20}}>
+          {currentSlideIndex == slides.length - 1 ? (
+            <View style={{height: 50}}>
+              <TouchableOpacity
+                style={styles.btn}
+                onPress={() => navigation.replace('HomeScreen')}>
+                <Text style={{fontWeight: 'bold', fontSize: 15}}>
+                  GET STARTED
+                </Text>
+              </TouchableOpacity>
+            </View>
+          ) : (
+            <View style={{flexDirection: 'row'}}>
+              <TouchableOpacity
+                activeOpacity={0.8}
+                style={[
+                  styles.btn,
+                  {
+                    borderColor: COLORS.white,
+                    borderWidth: 1,
+                    backgroundColor: 'transparent',
+                  },
+                ]}
+                onPress={skip}>
+                <Text
+                  style={{
+                    fontWeight: 'bold',
+                    fontSize: 15,
+                    color: COLORS.white,
+                  }}>
+                  SKIP
+                </Text>
+              </TouchableOpacity>
+              <View style={{width: 15}} />
+              <TouchableOpacity
+                activeOpacity={0.8}
+                onPress={goToNextSlide}
+                style={styles.btn}>
+                <Text
+                  style={{
+                    fontWeight: 'bold',
+                    fontSize: 15,
+                  }}>
+                  NEXT
+                </Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        </View>
+      </View>
+    );
+  };
+
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor: COLORS.primary}}>
+      <StatusBar backgroundColor={COLORS.primary} />
+      <FlatList
+        ref={ref}
+        onMomentumScrollEnd={updateCurrentSlideIndex}
+        contentContainerStyle={{height: height * 0.75}}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        data={slides}
+        pagingEnabled
+        renderItem={({item}) => <Slide item={item} />}
+      />
+      <Footer />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  subtitle: {
+    color: COLORS.white,
+    fontSize: 13,
+    marginTop: 10,
+    maxWidth: '70%',
+    textAlign: 'center',
+    lineHeight: 23,
+  },
+  title: {
+    color: COLORS.white,
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginTop: 20,
+    textAlign: 'center',
+  },
+  image: {
+    height: '100%',
+    width: '100%',
+    resizeMode: 'contain',
+  },
+  indicator: {
+    height: 2.5,
+    width: 10,
+    backgroundColor: 'grey',
+    marginHorizontal: 3,
+    borderRadius: 2,
+  },
+  btn: {
+    flex: 1,
+    height: 50,
+    borderRadius: 5,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 export default OnBoarding;
