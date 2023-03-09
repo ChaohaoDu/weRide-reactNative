89fbc1cf40649849f14d3e026f73100ef880bb87
Chaohao Du <chaohaodu@gmail.com>
2023-03-08 23:59:29 -0800

    complete single activity record board

--- App.js
+++ App.js
@@ -4,10 +4,18 @@ import Navigation from './src/navigation/navigation';
 import Verification from './src/screens/Verification';
 import Login from './src/screens/Login';
 import setCustomText from './src/utils/setCustomText';
-import {View} from 'react-native';
+import {SafeAreaView, View} from 'react-native';
+import ActivityBoard from './src/screens/ActivityBoard';
 setCustomText();
 const App: () => Node = () => {
-  return <Navigation />;
+  return (
+    <SafeAreaView>
+      <View style={{padding: 10}}>
+        <ActivityBoard />
+      </View>
+    </SafeAreaView>
+  );
+  // return <Navigation />;
   // return <Verification />;
   // return <Login />;
 };
--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,5 +1,12 @@
 import React, {useState} from 'react';
-import {Image, Linking, SafeAreaView, TouchableOpacity, Text, View} from 'react-native';
+import {
+  Image,
+  Linking,
+  SafeAreaView,
+  TouchableOpacity,
+  Text,
+  View,
+} from 'react-native';
 import {deleteJWToken} from '../../utils/jwt';
 import {AuthContext} from '../../navigation/navigation';
 import styles from './style';
@@ -8,30 +15,25 @@ import RoleContainer from '../../components/RoleContainer';
 const Home = () => {
   const auth = React.useContext(AuthContext);
   const logoUrl = require('../../assets/images/logo/logo_text_at_bottom.png');
-  const driverURL = require("../../assets/images/choose_your_role/driver.png");
-  const riderURL = require("../../assets/images/choose_your_role/rider.png");
+  const driverURL = require('../../assets/images/choose_your_role/driver.png');
+  const riderURL = require('../../assets/images/choose_your_role/rider.png');
   const onLogoutPressed = async () => {
     await deleteJWToken();
     auth.signOut();
   };
   const [selectedImage, setSelectedImage] = useState(null);
 
-  const handleImagePress = (image) => {
+  const handleImagePress = image => {
     setSelectedImage(image);
   };
-
 
   return (
     <SafeAreaView>
       <View style={styles.root}>
         <Image source={logoUrl} style={styles.logo} />
-        <Text style = {styles.title1}>Choose</Text>
-        <Text style = {styles.title2}>Your Role</Text>
-        <RoleContainer 
-          pictureURL={driverURL}
-          role = {"Drive"}
-          />
-
+        <Text style={styles.title1}>Choose</Text>
+        <Text style={styles.title2}>Your Role</Text>
+        <RoleContainer pictureURL={driverURL} role={'Drive'} />
       </View>
     </SafeAreaView>
   );
